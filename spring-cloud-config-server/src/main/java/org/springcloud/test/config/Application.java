de171fe72c1f7e821d7233a0c16bf19fd7f4832f
huangjiangnan <huangjiangnanjava@163.com>
2018-03-07 18:47:56 +0800

    配置中心

--- spring-cloud-config-server/src/main/java/org/springcloud/test/config/Application.java
+++ spring-cloud-config-server/src/main/java/org/springcloud/test/config/Application.java
@@ -2,15 +2,12 @@ package org.springcloud.test.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
-@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
-@EnableScheduling
 @ComponentScan(basePackages = "org.springcloud.test.config")
 public class Application extends WebMvcConfigurerAdapter {
 
